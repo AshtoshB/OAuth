fb367f18c99b6ef73a603fe797706d7daf370589
Ashtosh Bhandari <ashtosh@bu.edu>
2025-04-01 23:11:18 -0400

    Final Submit

---
+++ OAUTH FINAL FINAL/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace OAUTH_FINAL_FINAL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GoogleSign_BTN = new System.Windows.Forms.Button();
+            this.SignOut_BTN = new System.Windows.Forms.Button();
+            this.UserProfile_PB = new System.Windows.Forms.PictureBox();
+            this.Status_lb = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.UserProfile_PB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GoogleSign_BTN
+            // 
+            this.GoogleSign_BTN.Location = new System.Drawing.Point(303, 373);
+            this.GoogleSign_BTN.Name = "GoogleSign_BTN";
+            this.GoogleSign_BTN.Size = new System.Drawing.Size(164, 55);
+            this.GoogleSign_BTN.TabIndex = 0;
+            this.GoogleSign_BTN.Text = "Google Sign In";
+            this.GoogleSign_BTN.UseVisualStyleBackColor = true;
+            this.GoogleSign_BTN.Click += new System.EventHandler(this.GoogleSign_BTN_Click);
+            // 
+            // SignOut_BTN
+            // 
+            this.SignOut_BTN.Location = new System.Drawing.Point(303, 374);
+            this.SignOut_BTN.Name = "SignOut_BTN";
+            this.SignOut_BTN.Size = new System.Drawing.Size(164, 54);
+            this.SignOut_BTN.TabIndex = 1;
+            this.SignOut_BTN.Text = "Sign Out";
+            this.SignOut_BTN.UseVisualStyleBackColor = true;
+            this.SignOut_BTN.Visible = false;
+            this.SignOut_BTN.Click += new System.EventHandler(this.SignOut_BTN_Click);
+            // 
+            // UserProfile_PB
+            // 
+            this.UserProfile_PB.Location = new System.Drawing.Point(303, 34);
+            this.UserProfile_PB.Name = "UserProfile_PB";
+            this.UserProfile_PB.Size = new System.Drawing.Size(164, 124);
+            this.UserProfile_PB.TabIndex = 2;
+            this.UserProfile_PB.TabStop = false;
+            this.UserProfile_PB.Click += new System.EventHandler(this.UserProfile_PB_Click);
+            // 
+            // Status_lb
+            // 
+            this.Status_lb.AutoSize = true;
+            this.Status_lb.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Status_lb.Location = new System.Drawing.Point(221, 224);
+            this.Status_lb.Name = "Status_lb";
+            this.Status_lb.Size = new System.Drawing.Size(320, 52);
+            this.Status_lb.TabIndex = 3;
+            this.Status_lb.Text = "Not Logged In ";
+            this.Status_lb.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 450);
+            this.Controls.Add(this.Status_lb);
+            this.Controls.Add(this.UserProfile_PB);
+            this.Controls.Add(this.SignOut_BTN);
+            this.Controls.Add(this.GoogleSign_BTN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.UserProfile_PB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GoogleSign_BTN;
+        private System.Windows.Forms.Button SignOut_BTN;
+        private System.Windows.Forms.PictureBox UserProfile_PB;
+        private System.Windows.Forms.Label Status_lb;
+    }
+}
+
---
+++ OAUTH FINAL FINAL/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Util.Store;
+using Google.Apis.PeopleService.v1;
+using Google.Apis.PeopleService.v1.Data;
+using Google.Apis.Services;
+
+
+namespace OAUTH_FINAL_FINAL
+{
+    public partial class Form1 : Form
+    {
+        // Scopes needed for accessing user profile and email
+        static string[] Scopes = { "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile" };
+        static string ApplicationName = "Google OAuth WinForms App";
+        UserCredential credential;
+
+        //Client ID and Secret
+        private const string ClientId = "77798700369-ncovj9o6cai378k4b67ac8melo1mlbbo.apps.googleusercontent.com";
+        private const string ClientSecret = "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";
+        private const string RedirectUri = "http://localhost";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void GoogleSign_BTN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Step 1: Create the OAuth2 credentials using hardcoded Client ID and Secret
+                var clientSecrets = new ClientSecrets
+                {
+                    ClientId = ClientId,
+                    ClientSecret = ClientSecret
+                };
+
+                //Step 2: Authorize the request 
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    clientSecrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore("GoogleOAuthCredentials", true) // Store credentials
+                );
+
+                GoogleSign_BTN.Visible = false;
+                GoogleSign_BTN.Enabled = false;
+
+                // Stpe 3: fetch user data from Google People API
+                var service = new PeopleServiceService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = ApplicationName
+                });
+
+                // Getting user profile information (name, email, and profile picture)
+                var profileRequest = service.People.Get("people/me");
+                profileRequest.PersonFields = "names,emailAddresses,photos";
+                Person profile = await profileRequest.ExecuteAsync();
+
+
+                string userName = profile.Names?[0]?.DisplayName ?? "Unknown";
+                string userEmail = profile.EmailAddresses?[0]?.Value ?? "Unknown";
+
+                // Trying to get the user profile picture
+                try
+                {
+                    // Try fetching the profile picture URL
+                    string userProfilePictureUrl = profile.Photos?[0]?.Url ?? "No profile picture";
+
+                    // Try loading the profile picture into a PictureBox if a URL is available
+                    if (!string.IsNullOrEmpty(userProfilePictureUrl) && userProfilePictureUrl != "No profile picture")
+                    {
+                        UserProfile_PB.Load(userProfilePictureUrl);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading profile picture: {ex.Message}");
+                }
+
+                Status_lb.Location = new Point(Status_lb.Location.X - 50, Status_lb.Location.Y);
+                Status_lb.Font = new Font("Microsoft Sans Serif", 20);
+                Status_lb.Text = $"Welcome {userName}!\nEmail: {userEmail}";
+               
+                
+
+                SignOut_BTN.Enabled = true;
+                SignOut_BTN.Visible = true;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Login failed: " + ex.Message);
+            }
+
+        }
+
+        private void SignOut_BTN_Click(object sender, EventArgs e)
+        {
+            string credentialsFolderPath = Path.Combine(Application.StartupPath, "GoogleOAuthCredentials");
+
+            // Check if the directory exists
+            if (Directory.Exists(credentialsFolderPath))
+            {
+                // Delete all files inside the folder
+                var files = Directory.GetFiles(credentialsFolderPath);
+                foreach (var file in files)
+                {
+                    File.Delete(file); // Delete the stored file
+                }
+
+                // Optionally, delete the folder if it is empty
+                Directory.Delete(credentialsFolderPath);
+            }
+
+            credential = null;
+
+            Status_lb.Location = new Point(Status_lb.Location.X + 50, Status_lb.Location.Y);
+            Status_lb.Font = new Font("Microsoft Sans Serif", 25);
+            Status_lb.Text = "Not Logged In";
+
+            GoogleSign_BTN.Visible = true;
+            GoogleSign_BTN.Enabled = true;
+
+            SignOut_BTN.Enabled = false;
+            SignOut_BTN.Visible = false;
+            UserProfile_PB.Image = null;
+
+            MessageBox.Show("Logged out successfully!");
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserProfile_PB_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
